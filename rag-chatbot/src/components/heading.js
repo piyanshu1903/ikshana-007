1cb0f656d0df637d7d144728d510bacbb2d2d5d4
Yogesh3052 <magaryogesh47783@gmail.com>
2024-09-09 01:03:06 +0530

    light gone so little css done

--- rag-chatbot/src/components/heading.js
+++ rag-chatbot/src/components/heading.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/header.css";
+import "../styles/App.css";
 
 function Header() {
   return (
--- rag-chatbot/src/components/inputForm.js
+++ rag-chatbot/src/components/inputForm.js
@@ -1,8 +1,12 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleUp, faPaperclip } from '@fortawesome/free-solid-svg-icons';
 const InputForm = ({ input, setInput, handleSubmit, disabled }) => {
   return (
     <form onSubmit={handleSubmit} className="input-form">
+      <button disabled={disabled}>
+        <FontAwesomeIcon icon={faPaperclip} size="2x" color="white"/>
+      </button>
       <input
         type="text"
         value={input}
@@ -10,8 +14,8 @@ const InputForm = ({ input, setInput, handleSubmit, disabled }) => {
         placeholder="Type a message..."
         disabled={disabled} // Disable input field if disabled is true
       />
-      <button type="submit" disabled={disabled}>
-        Send
+      <button  disabled={disabled}>
+        <FontAwesomeIcon icon={faArrowCircleUp } size="2x"color="White"/>
       </button>
     </form>
   );
